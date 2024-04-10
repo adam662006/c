54385714d4a02b650b26b10c305f6d7469a08b55
Adam Martinez <adammartinezoussat@gmail.com>
2024-04-11 00:58:40 +0100

    feat: change exercises from 1st edition to 2nd

--- e1-5.c
+++ e1-5.c
@@ -4,7 +4,7 @@ main() {
 	int fahr;
 
 	printf("Fahrenheit to Celsius conversion\n");
-	for (fahr = 20; fahr <= 300; fahr = fahr + 10) {
+	for (fahr = 300; fahr >= 20; fahr = fahr - 10) {
 		printf("Fahrenheit: %4d Celsius: %4.1f\n", fahr, (5.0/9.0) * (fahr - 32));
 	}
 }
